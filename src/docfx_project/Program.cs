ef0d6472bb556c820655ebe17712b42123f53ba9
David Vreony <dpvreony@users.noreply.github.com>
2025-07-07 23:48:16 +0100

    add in net logger

--- src/docfx_project/Program.cs
+++ src/docfx_project/Program.cs
@@ -4,6 +4,8 @@ using Dhgms.DocFx.MermaidJs.Plugin.Markdig;
 using Microsoft.DocAsCode;
 using Microsoft.DocAsCode.Dotnet;
 using Microsoft.DocAsCode.MarkdigEngine.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Whipstaff.Markdig.Settings;
 using Whipstaff.Playwright;
@@ -27,13 +29,18 @@ namespace docfx_project
                 const string configPath = "docfx.json";
                 await DotnetApiCatalog.GenerateManagedReferenceYamlFiles(configPath).ConfigureAwait(false);
 
+                var serviceCollection = new ServiceCollection();
+                serviceCollection.AddLogging(loggingBuilder => loggingBuilder.AddConsole());
+                var serviceProvider = serviceCollection.BuildServiceProvider();
+                var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
                 var settings =
                     new MarkdownJsExtensionSettings(PlaywrightBrowserTypeAndChannel.Chrome(), OutputMode.Png);
 
                 var options = new BuildOptions
                 {
                     // Enable MermaidJS markdown extension
-                    ConfigureMarkdig = pipeline => pipeline.UseMermaidJsExtension(settings, new NullLoggerFactory())
+                    ConfigureMarkdig = pipeline => pipeline.UseMermaidJsExtension(settings, loggerFactory)
                 };
 
                 await Docset.Build(configPath, options).ConfigureAwait(false);
